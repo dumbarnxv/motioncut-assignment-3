f812b8ee326c0cc1adc1b6ece64ce08e46f31b77
ARNAV KUMAR SINAH <157496721+dumbarnxv@users.noreply.github.com>
2024-08-24 23:12:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".multi", {
+  strings: [
+    "NOW AVAILABLE AT OUR BESTO STORE!",
+    "NOW AVAILABLE AT OUR BESTO STORE!",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
